0512fdccb628cc8b17250717ecbf854ec6ec1b1b
bobby-rust <bobby.rust121@gmail.com>
2024-11-06 16:27:46 -0500

    Module 2 completed baseline

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const handlebars = require("hbs");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const indexRouter = require("./app_server/routes/index");
+const usersRouter = require("./app_server/routes/users");
+var travelRouter = require("./app_server/routes/travel");
 
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "app_server", "views"));
 app.set("view engine", "hbs");
+
+// register partials
+handlebars.registerPartials(__dirname + "/app_server/views/partials");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -21,6 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/travel", travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
