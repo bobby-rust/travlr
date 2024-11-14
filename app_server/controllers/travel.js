b6c9aac9fa22d0df82707807a244d1d2559cdcc8
bobby-rust <bobby.rust121@gmail.com>
2024-11-14 09:33:28 -0500

    module 3 - read data from json file

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,9 @@
+var fs = require("fs");
+var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf-8")).data;
+console.log("TRIPS: ", trips);
 // GET travel view
 const travel = (req, res) => {
-    res.render("travel", { title: "Travlr Getaways" });
+    res.render("travel", { data: trips });
 };
 
 module.exports = { travel };
